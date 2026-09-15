9ac10e116135b79b84e62eab1acff0ab50055362
Aryan Raj <157047439+Aryan3572@users.noreply.github.com>
2026-09-15 14:35:28 +0530

    Time: 1787 ms (5%), Space: 0B (100%) - LeetHub

---
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -0,0 +1,15 @@
+select Signups.user_id ,
+       round (
+            ifnull(
+                avg(
+                    case 
+                     when Confirmations.action = 'confirmed' then 1
+                     else 0
+                    end
+                )
+            ,0)
+        ,2) as confirmation_rate
+from Signups
+left join Confirmations
+on Signups.user_id = Confirmations.user_id
+group by Signups.user_id;
